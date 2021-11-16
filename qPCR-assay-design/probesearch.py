d4c26ad9f1b70b4db7e18e8ffc8bc173f9d753e8
cherrytrees-kpu <54606285+cherrytrees-kpu@users.noreply.github.com>
2021-11-16 15:14:35 -0800

    Update probesearch.py
    1) changed check_primer to check_probe
    2) global variables removed
    3) get_probes has two additional arguments: min_primer_len, max_primer_len
    4) changed input to be 1-based coordinates, and 1-based coordinates are converted to 0-based coordinates prior to use
    5) generate_blast_result_db now accepts blastdb argument
    6) blast now accepts blastdb argument
    7) num_alignments removed from blast command
    8) added several arguments to parse_args() function
    9) added ability for script to be run without sensitivity and specificity checking

--- qPCR-assay-design/probesearch.py
+++ qPCR-assay-design/probesearch.py
@@ -7,12 +7,7 @@ import subprocess
 import pandas
 import csv
 
-MIN_PRIMER_LEN = 17
-MAX_PRIMER_LEN = 22
-LEN_DB = 16194
-BLASTDB = 'SSU_nematode.fasta'
-
-def check_primer(seq): 
+def check_probe(seq): 
     def count_c_g(seq): 
         return seq.count('c') + seq.count('g') + seq.count('C') + seq.count('G')
     def chk_run(seq): 
@@ -53,7 +48,7 @@ def check_primer(seq):
     else: 
         return False
 
-def get_probes(target_sequence, target_start): 
+def get_probes(target_sequence, target_start, min_primer_len, max_primer_len): 
     """
     Function finds all viable probes. 
     Probe criteria: 
@@ -64,28 +59,29 @@ def get_probes(target_sequence, target_start):
     """
     list_probes = []
     #For each root position in the target sequence
-    for i in range(len(target_sequence) - MIN_PRIMER_LEN + 1):
+    for i in range(len(target_sequence) - min_primer_len + 1):
         #Iterate over all probe lengths
-        for probe_len in range(MIN_PRIMER_LEN, MAX_PRIMER_LEN + 1): 
+        for probe_len in range(min_primer_len, max_primer_len + 1): 
             probe_seq = target_sequence[i:i+probe_len]
-            if check_primer(probe_seq) is True: 
+            if check_probe(probe_seq) is True: 
+                #Note that the coordinates are converted back to 1-based
                 list_probes.append(
                     {
-                        'root_pos':target_start + i,
+                        'root_pos':target_start + i + 1,
                         'len':probe_len,
                         'seq':str(probe_seq),
                     }
                 )
     return list_probes
 
-def generate_blast_results_db(list_probes): 
+def generate_blast_results_db(list_probes, blastdb): 
     blast_results = dict()
     for probe in list_probes:
         probe_id = f"{str(probe['root_pos'])}-{probe['len']}" 
-        blast_results[probe_id] = blast(probe['seq'], BLASTDB, LEN_DB)
+        blast_results[probe_id] = blast(probe['seq'], blastdb)
     return blast_results
 
-def blast(seq, blastdb, num_aligned): 
+def blast(seq, blastdb): 
     fasta = tempfile.NamedTemporaryFile(delete=True)
     fasta.write(f">probe\n{str(seq)}".encode())
     fasta.seek(0)
@@ -95,8 +91,6 @@ def blast(seq, blastdb, num_aligned):
         "blastn-short",
         "-db",
         blastdb,
-        "-num_alignments",
-        str(num_aligned),
         "-outfmt",
         "10 qacc sacc ssciname pident qlen length mismatch gapopen qstart qend sstart send evalue bitscore",
         "-query",
@@ -179,113 +173,154 @@ def calculate_specificity(blast_results, target_accessions, blastdb_len):
 def parse_args(): 
     parser = argparse.ArgumentParser(description='probesearch.py - identify viable probes in an alignment for given target sequences')
     parser.add_argument('target_seq', 
-        metavar='genus_alignments', 
         action='store', 
-        type=str,
-        help = 'path to species alignments directory'
-    )
-    parser.add_argument('target_accessions', 
-        metavar='target_accessions', 
-        action='store', 
-        type=str,
-        help = 'path to target_accessions'
+        type=pathlib.Path,
+        help = 'Path to target sequence file, fasta format'
     )
     parser.add_argument('target_start',
         metavar='target_start', 
         action='store',
         type=int,
-        help='start coordinate of target region'
+        help='Start coordinate of target region, 1-based coordinates'
     )
     parser.add_argument('target_end',
         metavar='target_end', 
         action='store',
         type=int,
-        help='end coordinate of target region'
+        help='End coordinate of target region, 1-based coordinates'
+    )
+    parser.add_argument('-min_primer_len',
+        action='store',
+        type=int, 
+        default=17,
+        dest='min_primer_len',
+        help='Minimum primer length'
+    )
+    parser.add_argument('-max_primer_len',
+        action='store',
+        type=int, 
+        default=22,
+        dest='max_primer_len',
+        help='Maximum primer length'
+    )
+    parser.add_argument('-no_sens_spec_check',
+        action='store_true',
+        dest='sens_spec_flag',
+        help='Flag to not check the putative probes for their specificity and sensitivity'
+    )
+    #Arguments for specificity checking
+    parser.add_argument('-blastdb',
+        action='store',
+        type=str,
+        dest='blastdb',
+        default='',
+        help='Name of blastdb'
+    )
+    parser.add_argument('-blastdb_len',
+        action='store',
+        type=int,
+        dest='blastdb_len',
+        help='Length of blastdb'
+    )
+    parser.add_argument('-target_accessions', 
+        metavar='target_accessions', 
+        action='store', 
+        type=pathlib.Path,
+        dest='target_accessions',
+        default='',
+        help = 'Path to target_accessions'
     )
     args = parser.parse_args()
-    target_seq_path = pathlib.Path(args.target_seq)
-    target_accessions_path = pathlib.Path(args.target_accessions)
-    return (target_seq_path, target_accessions_path, args.target_start, args.target_end)
-def main(target_seq_path, target_accessions, target_start, target_end):
-    headers=[
-        'qacc',
-        'sacc',
-        'ssciname',
-        'pident',
-        'qlen',
-        'length',
-        'mismatch', 
-        'gapopen', 
-        'qstart', 
-        'qend', 
-        'sstart', 
-        'send', 
-        'evalue', 
-        'bitscore',
-    ]
+    #Argument order: target sequence, target start coordinate, target end coordinate, minimum primer length, max primer length, check flag, blastdb, target accession path
+    #Note that coordinates are converted to 0-based half-open coordinates
+    return (args.target_seq, args.target_start-1, args.target_end, args.min_primer_len, args.max_primer_len, args.sens_spec_flag, args.blastdb, args.blastdb_len, args.target_accessions)
+
+def main(target_seq_path, target_start, target_end, min_primer_len, max_primer_len, check_flag, blastdb, blastdb_len, target_accession_path):
     #Input data - alignment, target indices
     target_seq_file = SeqIO.read(target_seq_path, 'fasta')
     target_sequence = target_seq_file.seq[target_start:target_end]
     print(len(target_sequence))
     #Generate probes
-    list_probes = get_probes(str(target_sequence), target_start)
-    blast_results = generate_blast_results_db(list_probes)
-
-    #Calculate sensitivity and specificity for each probe
-    for probe in list_probes: 
-        probe_id = f"{str(probe['root_pos'])}-{probe['len']}" 
-        probe_blast_results = blast_results[probe_id]
-        probe['sensitivity'] = calculate_sensitivity(probe_blast_results, target_accessions)
-        probe['specificity'] = calculate_specificity(probe_blast_results, target_accessions, LEN_DB)
-        probe['score'] = probe['sensitivity'] + probe['specificity']
-    
-    #Sort
-    list_probes.sort(key=lambda x: x['score'], reverse=True)
-
-    #Outputs:
-    #1) blast_csv for each blast result for each primer
-    #2) file listing all of the probes
-    #Blast results
-    blast_folder_path = target_seq_path.with_name('probe_blast')
-    blast_folder_path.mkdir()
-    for blast_result_key in blast_results: 
-        blast_output_path = blast_folder_path.joinpath(f"{blast_result_key}_probe.csv")
-        blast_output_file = open(blast_output_path, 'w')
-        blast_results[blast_result_key].to_csv(blast_output_file)
-        blast_output_file.close()
-    #Probes
-    probe_data = []
-    for probe in list_probes: 
-        probe_data.append(
+    list_probes = get_probes(str(target_sequence), target_start, min_primer_len, max_primer_len)
+    #CALCULATE SENSITIVITY AND SPECIFICITY
+    if check_flag is False: 
+        #Execute BLAST
+        blast_results = generate_blast_results_db(list_probes, blastdb)
+        #Read target accessions
+        target_accessions = []
+        input_file = open(target_accession_path, 'r')
+        for line in input_file: 
+            target_accessions.append(line.strip('\n'))
+        input_file.close()
+        #Calculate sensitivity and specificity for each probe
+        for probe in list_probes: 
+            probe_id = f"{str(probe['root_pos'])}-{probe['len']}" 
+            probe_blast_results = blast_results[probe_id]
+            probe['sensitivity'] = calculate_sensitivity(probe_blast_results, target_accessions)
+            probe['specificity'] = calculate_specificity(probe_blast_results, target_accessions, blastdb_len)
+            probe['score'] = probe['sensitivity'] + probe['specificity']
+        #Sort
+        list_probes.sort(key=lambda x: x['score'], reverse=True)
+        #Outputs:
+        #1) blast_csv for each blast result for each primer
+        #2) file listing all of the probes
+        #Blast results
+        blast_folder_path = target_seq_path.with_name('probe_blast')
+        blast_folder_path.mkdir()
+        for blast_result_key in blast_results: 
+            blast_output_path = blast_folder_path.joinpath(f"{blast_result_key}_probe.csv")
+            blast_output_file = open(blast_output_path, 'w')
+            blast_results[blast_result_key].to_csv(blast_output_file)
+            blast_output_file.close()
+        #Probes
+        probe_data = []
+        for probe in list_probes: 
+            probe_data.append(
+                (
+                    probe['root_pos'],
+                    probe['len'], 
+                    probe['seq'],
+                    probe['sensitivity'],
+                    probe['specificity'],
+                    probe['score'],
+                )
+            )
+        csv_output_path = target_seq_path.with_name('probe_candidates.csv')
+        csvfile = open(csv_output_path, 'w', newline='')
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(
             (
-                probe['root_pos'],
-                probe['len'], 
-                probe['seq'],
-                probe['sensitivity'],
-                probe['specificity'],
-                probe['score'],
+                'probe_root',
+                'probe_len',
+                'probe_seq',
+                'sens',
+                'spec',
+                'score',
             )
         )
-    csv_output_path = target_seq_path.with_name('probe_candidates.csv')
-    csvfile = open(csv_output_path, 'w', newline='')
-    csvwriter = csv.writer(csvfile)
-    csvwriter.writerow(
-        (
-            'probe_root',
-            'probe_len',
-            'probe_seq',
-            'sens',
-            'spec',
-            'score',
+        csvwriter.writerows(probe_data)
+        csvfile.close()
+    else:
+        csv_output_path = target_seq_path.with_name('probe_candidates.csv')
+        csvfile = open(csv_output_path, 'w', newline='')
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(
+            (
+                'probe_root',
+                'probe_len',
+                'probe_seq',
+            )
         )
-    )
-    csvwriter.writerows(probe_data)
-    csvfile.close()
+        probe_data = []
+        for probe in list_probes: 
+            probe_data.append(
+                (
+                    probe['root_pos'],
+                    probe['len'], 
+                    probe['seq'],
+                )
+            )
+        csvwriter.writerows(probe_data)
 if __name__ == '__main__': 
-    target_seq_path, target_accessions_path, target_start, target_end = parse_args()
-    target_accessions = []
-    input_file = open(target_accessions_path, 'r')
-    for line in input_file: 
-        target_accessions.append(line.strip('\n'))
-    input_file.close()
-    main(target_seq_path, target_accessions, target_start, target_end)
+    target_seq_path, target_start, target_end, min_primer_len, max_primer_len, check_flag, blastdb, blastdb_len, target_accession_path = parse_args()
+    main(target_seq_path, target_start, target_end, min_primer_len, max_primer_len, check_flag, blastdb, blastdb_len, target_accession_path)

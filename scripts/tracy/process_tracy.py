8cb0b12875ccb44090e8fad536fbcfe2fd68c743
cherrytrees-kpu <54606285+cherrytrees-kpu@users.noreply.github.com>
2021-12-21 16:50:12 -0800

    Update process_tracy.py
    First version where metadata and variants are exported

--- scripts/tracy/process_tracy.py
+++ scripts/tracy/process_tracy.py
@@ -1,6 +1,7 @@
 import json
 import argparse
 import pathlib
+import csv
 
 def parse_metadata(all_seq_data): 
     trim_parameters = []
@@ -8,8 +9,27 @@ def parse_metadata(all_seq_data):
         seq_len = len(all_seq_data[cow]['primarySeq'])
         trimleft = all_seq_data[cow]['meta']['arguments']['trimLeft']
         trimright = all_seq_data[cow]['meta']['arguments']['trimRight']
-        trim_parameters.append((cow, trimleft, trimright))
-    print(trim_parameters)
+        adjusted_seq_len = seq_len - trimleft - trimright
+        trim_parameters.append((cow, seq_len, adjusted_seq_len, trimleft, trimright))
+    return trim_parameters
+
+def parse_variants(all_seq_data): 
+    variants = []
+    for cow in all_seq_data: 
+        list_variants = all_seq_data[cow]['variants']['rows']
+        if len(list_variants) > 0: 
+            for variant in list_variants:
+                variant.insert(0, cow)
+                variants.append(variant)
+    return variants
+
+def output_csv(data, file_name, file_path, header): 
+    output_path = file_path.with_name(file_name+'.csv')
+    output_file = open(output_path, 'w', newline='')
+    csv_writer = csv.writer(output_file, dialect='excel')
+    csv_writer.writerow(header)
+    csv_writer.writerows(data)
+    output_file.close()
 
 def parse_args(): 
     parser = argparse.ArgumentParser(description='Extract variant calls from Tracy JSON file')
@@ -33,7 +53,33 @@ def main(json_path):
             all_seq_data[cow] = seq_data
             input_file.close()
     
-    parse_metadata(all_seq_data)
+    metadata = parse_metadata(all_seq_data)
+    variants = parse_variants(all_seq_data)
+
+    #Output
+    metadata_header = [
+        'Sample',
+        'Raw Sequence Length',
+        'Trimmed Sequence Length', 
+        'Left Trim', 
+        'Right Trim',
+    ]
+    variants_header = [
+        'Sample',
+        'chr',
+        'pos',
+        'id',
+        'ref',
+        'alt',
+        'qual',
+        'filter',
+        'type',
+        'genotype',
+        'basepos',
+        'signalpos',
+    ]
+    output_csv(metadata, 'metadata', json_path, metadata_header)
+    output_csv(variants, 'variants', json_path, variants_header)
 
 if __name__ == '__main__': 
     json_path = parse_args()

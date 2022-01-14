94714f01392397072bbc1e9a9ae51016981ca8d6
cherrytrees-kpu <54606285+cherrytrees-kpu@users.noreply.github.com>
2022-01-14 15:21:47 -0800

    Update batch_tracy.py
    1) Changed parse_args location
    2) Implemented left trim and right trim based on mott's algorithm of trimming

--- scripts/tracy/batch_tracy.py
+++ scripts/tracy/batch_tracy.py
@@ -1,8 +1,16 @@
+from Bio import SeqIO
 import argparse
 import subprocess
 import pathlib
 
-def run_tracy(gene_name, sample_id, reference_fasta_path, ab1_file_path, peak_ratio, trim): 
+def find_trim_values(ab1_file_path):
+    untrimmed = SeqIO.read(ab1_file_path, 'abi')
+    trimmed = SeqIO.read(ab1_file_path, 'abi-trim')
+    left_trim = untrimmed.seq.find(trimmed.seq[0:5])-1
+    right_trim = len(untrimmed.seq) - len(trimmed) - left_trim
+    return left_trim, right_trim
+
+def run_tracy(gene_name, sample_id, reference_fasta_path, ab1_file_path, peak_ratio, left_trim, right_trim): 
     """
     Run tracy using the reference fasta files
     """
@@ -13,11 +21,15 @@ def run_tracy(gene_name, sample_id, reference_fasta_path, ab1_file_path, peak_ra
         reference_fasta_path,
         '-v',
         '-t',
-        trim,
+        '0',
         '-p',
         peak_ratio,
         '-o',
         f'{gene_name}_{sample_id}',
+        '-q',
+        str(left_trim),
+        '-u',
+        str(right_trim), 
         ab1_file_path,
     ]
     result = subprocess.run(args, capture_output=False)
@@ -41,24 +53,20 @@ def parse_args():
         type=str,
         help='Peak ratio to pass to tracy. 0 to 1.'
     )
-    parser.add_argument('-t',
-        dest='trim',
-        default='3',
-        action='store',
-        type=str,
-        help='Trimming stringency to pass to Tracy. 1 to 9. 1 is most stringent.'
-    )
+
     args = parser.parse_args()
 
-    return (args.reference_file_path, args.ab1_path, args.peak_ratio, args.trim)
+    return (args.reference_file_path, args.ab1_path, args.peak_ratio)
 
-def main(reference_file_path, ab1_path, peak_ratio, trim): 
+def main(): 
+    
+    reference_file_path, ab1_path, peak_ratio = parse_args()
+    
     for ab1_file_path in ab1_path.glob('*.ab1'):
         gene_name = ab1_file_path.stem.split('_')[1] 
         cow_name = ab1_file_path.stem.split('_')[2]
-        run_tracy(gene_name, cow_name, reference_file_path, ab1_file_path, peak_ratio, trim)
+        left_trim, right_trim = find_trim_values(ab1_file_path)
+        run_tracy(gene_name, cow_name, reference_file_path, ab1_file_path, peak_ratio, left_trim, right_trim)
 
 if __name__ == '__main__': 
-    reference_file_path, ab1_path, peak_ratio, trim = parse_args()
-    main(reference_file_path, ab1_path, peak_ratio, trim)
-
+    main()
